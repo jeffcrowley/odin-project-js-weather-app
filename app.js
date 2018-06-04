ba4dc04ad25c549f8eb297a725a5e564425a3060
jeffcrowley <jeffcrowley@Jeffs-MacBook-Pro.local>
2018-06-04 10:07:03 -0700

    create initial HTML and initial JS constants/variables

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", (e) =>{
+
+   const mainContent = document.getElementByID("content")
+   const submitButton = document.getElementByID("zip-submit")
+   const preLoaderMain = document.createElement("div")
+   const preLoaderIndeterminate = document.createElement("div")
+
+   preLoaderMain.setAttribute("class", "progress")
+   preLoaderIndeterminate.setAttribute("class", "indeterminate")
+})
